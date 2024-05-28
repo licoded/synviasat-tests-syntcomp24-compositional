d40bec768794777c43384d4ad90df6c37cb04f9a
licoded <busy.li@foxmail.com>
2024-05-28 16:33:01 +0800

    my_mona_dfa: init

---
+++ my_mona_dfa.h
@@ -0,0 +1,126 @@
+#ifndef __MY_MONA_DFA__
+#define __MY_MONA_DFA__
+#include <iostream>
+#include <vector>
+#include <assert.h>
+#include <string>
+using namespace std;
+
+int string2int(string s)
+{
+    int res = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        res = res * 10 + s[i] - '0';
+    }
+    return res;
+}
+
+void read_intlist_from_string(string &s, vector<int> &res)
+{
+    int i = 0;
+    while (i < s.size())
+    {
+        while (i < s.size() && s[i]==' ') i++;
+        if (i == s.size()) break;
+        int j = i;
+        while (j < s.size() && s[j] != ' ') j++;
+        res.push_back(string2int(s.substr(i, j - i)));
+        i = j;
+    }
+}
+
+bool isEnd(int *arr)
+{
+    return arr[0] == -1 && arr[2] == 0;
+}
+
+struct MyMonaDFA{
+    int trans_num;
+    int states_num;
+    int vars_num;           // don't use it actually
+    int initial_stateid;
+    vector<int> final;      // type of each state
+    vector<int> behaviour;  // first move of each state
+    vector<int[3]> bdd;     // transition array
+
+    void readMyMonaDFA_core()
+    {
+        string s;
+        char ch;
+        int line_num = 0;
+        while(true)
+        {
+            line_num++; // start from 1
+            string line_pres[] = {
+                "",
+                "MONA DFA",
+                "number of variables: ",
+                "variables: ",
+                "orders: ", // NOT USED
+                "states: ",
+                "initial: ",
+                "bdd nodes: ",
+                "final: ",
+                "behaviour: ",
+                "bdd:",
+            };
+            string &curline_pre = line_pres[line_num];
+
+            if (line_num < 11)
+            {
+                getline(cin, s); cin.get(ch);
+                assert(s.substr(0, curline_pre.size()) == curline_pre);
+                s = s.substr(curline_pre.size());
+                switch (line_num)
+                {
+                    case 2:
+                        vars_num = string2int(s);
+                        break;
+                    case 5:
+                        states_num = string2int(s);
+                        break;
+                    case 6:
+                        initial_stateid = string2int(s);
+                        break;
+                    case 7:
+                        trans_num = string2int(s);
+                        break;
+                    case 8:
+                        read_intlist_from_string(s, final);
+                        break;
+                    case 9:
+                        read_intlist_from_string(s, behaviour);
+                        break;
+                    case 10:
+                        /* read transitions */
+                        int x, l, r;
+                        for (int i = 0; i < trans_num; i++) {
+                            cin >> x >> l >> r;
+                            bdd.push_back({x, l, r});
+                        }
+                        /* last line*/
+                        cin >> s;
+                        assert(s.substr(0,3) == "end");
+                        break;
+                    default:
+                        // do nothing
+                        break;
+                }
+            }
+        }
+    }
+
+    void *readMyMonaDFA(string &dot_filename)
+    {
+        FILE* original_stdout = stdout;
+        stdout = fopen(dot_filename.c_str(), "w");
+        // === real code BEGIN
+        readMyMonaDFA_core();
+        // === real code END
+        fclose(stdout);
+        stdout = original_stdout;
+    }
+};
+
+#endif
